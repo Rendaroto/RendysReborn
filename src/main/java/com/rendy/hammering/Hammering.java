fbf3929fd9a104194c2092dd51587f55e35e2de5
Rendaroto <57144956+Rendaroto@users.noreply.github.com>
2024-02-24 20:34:31 +0100

    fix merge

--- src/main/java/com/rendy/hammering/Hammering.java
+++ src/main/java/com/rendy/hammering/Hammering.java
@@ -3,14 +3,10 @@ package com.rendy.hammering;
 import com.mojang.logging.LogUtils;
 import com.rendy.hammering.events.HammerEvents;
 import com.rendy.hammering.init.*;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.*;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 import static com.rendy.hammering.ui.CreativeTab.TAB_REG;
@@ -22,37 +18,15 @@ public class Hammering
     public static final String MOD_ID = "hammering";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-<<<<<<< HEAD
     public Hammering(IEventBus modEventBus)
-=======
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> HAMMERING_TAB = TAB_REG.register(MOD_ID, () -> new CreativeModeTab.Builder(CreativeModeTab.Row.TOP, 1).withTabsBefore(
-            CreativeModeTabs.SEARCH).icon(() -> new ItemStack(HammerInit.NETHERITE_HAMMER.get())).displayItems((config, builder) -> {
-        builder.accept(HammerInit.WOODEN_HAMMER.get());
-        builder.accept(HammerInit.STONE_HAMMER.get());
-        builder.accept(HammerInit.IRON_HAMMER.get());
-        builder.accept(HammerInit.GOLDEN_HAMMER.get());
-        builder.accept(HammerInit.DIAMOND_HAMMER.get());
-        builder.accept(HammerInit.NETHERITE_HAMMER.get());
-    }).build());
-
-
-    public Hammering()
->>>>>>> dd970d9a1e156e702056c445144c6e93fb85c136
     {
         //INIZIO LOG
         LOGGER.info("Inizio loading");
 
-<<<<<<< HEAD
-        NeoForge.EVENT_BUS.register(HammerEvents.class);
+        modEventBus.register(HammerEvents.class);
 
         //ITEMS
         ItemInit.ITEMS.register(modEventBus);
-=======
-        LOGGER.info("KTM");
-        //LOGGER.info(modEventBus.toString());
-
-        Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(HammerEvents.class);
->>>>>>> dd970d9a1e156e702056c445144c6e93fb85c136
 
         //TOOLS
         PickaxeInit.PICKAXES_ITEMS.register(modEventBus);
@@ -63,12 +37,8 @@ public class Hammering
 
         //HAMMERS
         HammerInit.HAMMER_ITEMS.register(modEventBus);
-<<<<<<< HEAD
 
         //CREATIVE_TAB
-=======
-        //NeoForge.EVENT_BUS.register(this);
->>>>>>> dd970d9a1e156e702056c445144c6e93fb85c136
         TAB_REG.register(modEventBus);
 
         //LOG
