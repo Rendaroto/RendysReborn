a211b26b6e5bd9fe545f3d9079f1f4f20e1f1338
Rendaroto <57144956+Rendaroto@users.noreply.github.com>
2024-02-26 18:45:20 +0100

    Aggiunta Dirctory e prima recipe, fix nomi e oggetti mancanti

--- src/main/java/com/rendy/hammering/init/ItemInit.java
+++ src/main/java/com/rendy/hammering/init/ItemInit.java
@@ -10,9 +10,9 @@ public class ItemInit {
 
     public final static DeferredRegister.Items ITEMS = DeferredRegister.createItems(Hammering.MOD_ID);
 
-    public static final DeferredItem<Item> POWERED_NETHERITE_INGOT = ITEMS.register("powered_netherite",() -> new Item(new Item.Properties().stacksTo(32).fireResistant()));
-    public static final DeferredItem<Item> REINFORCED_NETHERITE_INGOT = ITEMS.register("reinforced_netherite",() -> new Item(new Item.Properties().stacksTo(32).fireResistant()));
-    public static final DeferredItem<Item> SUPER_NETHERITE_INGOT = ITEMS.register("super_netherite",() -> new Item(new Item.Properties().stacksTo(32).fireResistant()));
+    public static final DeferredItem<Item> POWERED_NETHERITE_INGOT = ITEMS.register("powered_netherite_ingot",() -> new Item(new Item.Properties().stacksTo(32).fireResistant()));
+    public static final DeferredItem<Item> REINFORCED_NETHERITE_INGOT = ITEMS.register("reinforced_netherite_ingot",() -> new Item(new Item.Properties().stacksTo(32).fireResistant()));
+    public static final DeferredItem<Item> SUPER_NETHERITE_INGOT = ITEMS.register("super_netherite_ingot",() -> new Item(new Item.Properties().stacksTo(32).fireResistant()));
 
     public static final DeferredItem<Item> NETHERSTAR_DUST = ITEMS.register("netherstar_dust",() -> new Item(new Item.Properties().stacksTo(64).fireResistant()));
 
