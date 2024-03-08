159427361e1f824156b3f9adcd7b969ea1397341
Rendaroto <57144956+Rendaroto@users.noreply.github.com>
2024-03-08 23:35:14 +0100

    Versione 1 finalizzata

--- src/main/java/com/rendy/rendys/init/ArmorMaterialInit.java
+++ src/main/java/com/rendy/rendys/init/ArmorMaterialInit.java
@@ -12,11 +12,11 @@ import net.minecraft.world.item.crafting.Ingredient;
 import java.util.function.Supplier;
 
 public enum ArmorMaterialInit implements ArmorMaterial {
-    COPPER("copper", 8, new int[]{ 8, 10, 8, 7}, 15,SoundEvents.ARMOR_EQUIP_IRON, 1f, 0.1f, () -> Ingredient.of(Items.COPPER_INGOT)),
-    EMERALD("emerald", 43, new int[]{ 43, 45, 43, 42 }, 15,SoundEvents.ARMOR_EQUIP_DIAMOND, 2.2f, 0.2f, () -> Ingredient.of(Items.COPPER_INGOT)),
-    POWERED_NETHERITE("powered_netherite", 53, new int[]{ 53, 55, 53, 52 }, 15,SoundEvents.ARMOR_EQUIP_NETHERITE, 3.1f, 0.28f, () -> Ingredient.of(ItemInit.POWERED_NETHERITE_INGOT)),
-    REINFORCED_NETHERITE("reinforced_netherite", 58, new int[]{ 58, 60, 58, 57 }, 15,SoundEvents.ARMOR_EQUIP_NETHERITE, 4f, 0.35f, () -> Ingredient.of(ItemInit.REINFORCED_NETHERITE_INGOT)),
-    SUPER_NETHERITE("super_netherite", 65, new int[]{ 65, 72, 71, 68 }, 15,SoundEvents.ARMOR_EQUIP_NETHERITE, 5f, 0.4f, () -> Ingredient.of(ItemInit.SUPER_NETHERITE_INGOT));
+    COPPER("copper", 8, new int[]{ 8, 10, 8, 7}, 10,SoundEvents.ARMOR_EQUIP_IRON, 1f, 0.1f, () -> Ingredient.of(Items.COPPER_INGOT)),
+    EMERALD("emerald", 43, new int[]{ 4, 9, 7, 4 }, 25,SoundEvents.ARMOR_EQUIP_DIAMOND, 2.2f, 0.2f, () -> Ingredient.of(Items.COPPER_INGOT)),
+    POWERED_NETHERITE("powered_netherite", 53, new int[]{ 6, 11, 9, 6 }, 35,SoundEvents.ARMOR_EQUIP_NETHERITE, 3.1f, 0.28f, () -> Ingredient.of(ItemInit.POWERED_NETHERITE_INGOT)),
+    REINFORCED_NETHERITE("reinforced_netherite", 58, new int[]{ 8, 13, 11, 8 }, 45,SoundEvents.ARMOR_EQUIP_NETHERITE, 4f, 0.35f, () -> Ingredient.of(ItemInit.REINFORCED_NETHERITE_INGOT)),
+    SUPER_NETHERITE("super_netherite", 65, new int[]{ 10, 16, 14, 10 }, 55,SoundEvents.ARMOR_EQUIP_NETHERITE, 5f, 0.4f, () -> Ingredient.of(ItemInit.SUPER_NETHERITE_INGOT));
 
     private final String name;
     private final int durabilityMultiplier;
--- src/main/java/com/rendy/rendys/init/item/HoeInit.java
+++ src/main/java/com/rendy/rendys/init/item/HoeInit.java
@@ -12,17 +12,17 @@ public class HoeInit {
     public final static DeferredRegister.Items HOE_ITEMS = DeferredRegister.createItems(Rendys.MOD_ID);
 
     public static final DeferredItem<HoeItem> COPPER_HOE = HOE_ITEMS.register  ("copper_hoe",
-            () -> new HoeItem(TiersInit.COPPER_TIER,1,1f,new Item.Properties()));
+            () -> new HoeItem(TiersInit.COPPER_TIER,1,-1f,new Item.Properties()));
     public static final DeferredItem<HoeItem> EMERLAD_HOE = HOE_ITEMS.register  ("emerald_hoe",
-            () -> new HoeItem(TiersInit.EMERALD_TIER,1,1f,new Item.Properties()));
+            () -> new HoeItem(TiersInit.EMERALD_TIER,1,-1f,new Item.Properties()));
 
     public static final DeferredItem<HoeItem> POWERED_NETHERITE_HOE = HOE_ITEMS.register  ("powered_netherite_hoe",
-            () -> new HoeItem(TiersInit.POWERED_NETHERITE_TIER,1,1f, new Item.Properties().fireResistant()));
+            () -> new HoeItem(TiersInit.POWERED_NETHERITE_TIER,1,-1f, new Item.Properties().fireResistant()));
 
     public static final DeferredItem<HoeItem> REINFORCED_NETHERITE_HOE = HOE_ITEMS.register  ("reinforced_netherite_hoe",
-            () -> new HoeItem(TiersInit.REINFORCED_NETHERITE_TIER,1,1f, new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
+            () -> new HoeItem(TiersInit.REINFORCED_NETHERITE_TIER,1,-1f, new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
 
     public static final DeferredItem<HoeItem> SUPER_NETHERITE_HOE = HOE_ITEMS.register  ("super_netherite_hoe",
-            () -> new HoeItem(TiersInit.SUPER_NETHERITE_TIER,1,1f, new Item.Properties().rarity(Rarity.UNCOMMON).fireResistant()));
+            () -> new HoeItem(TiersInit.SUPER_NETHERITE_TIER,1,-1f, new Item.Properties().rarity(Rarity.UNCOMMON).fireResistant()));
 
 }
